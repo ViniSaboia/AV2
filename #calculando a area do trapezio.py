c09120843a1b99113eb41258b9613ecf45a9e3e7
ViniSaboia <110861585+ViniSaboia@users.noreply.github.com>
2022-11-07 20:36:48 -0300

    Add files via upload

---
+++ #calculando a area do trapezio.py
@@ -0,0 +1,10 @@
+#calculando a area do trapezio
+def area_do_trapezio():
+    B=float(input('Informe valor da base maior: '))
+    b=float(input('Informe valor da base menor: '))
+    h=float(input('Informe o valor da altura: '))
+    a=2
+   
+    formula=((B+b) * h)/a
+
+print('A área do trapézio é: ', Formula)
